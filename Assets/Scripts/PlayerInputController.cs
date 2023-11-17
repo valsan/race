9432e79f75fa9a38712fe543675ffe5436de112f
neviovalsa <nevio.valsa@gmail.com>
2023-11-17 17:40:02 +0100

    improve inputs

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -6,15 +6,20 @@ public class PlayerInputController : MonoBehaviour
     [SerializeField] KartMovement _kart;
 
     private InputAction _jumpAction;
+    private InputAction _throttleAction;
 
     private PlayerInput _playerInput;
     private void Awake()
     {
         _playerInput = GetComponent<PlayerInput>();
         _jumpAction = _playerInput.actions["Jump"];
+        _throttleAction = _playerInput.actions["Gas"];
 
         _jumpAction.started += OnJumpPressed;
         _jumpAction.canceled += OnJumpReleased;
+
+        _throttleAction.started += OnJumpPressed;
+        _throttleAction.canceled += OnJumpReleased;
     }
 
     public void OnMove(InputValue value)
@@ -31,6 +36,19 @@ public class PlayerInputController : MonoBehaviour
     public void OnJumpReleased(InputAction.CallbackContext context)
     {
         Debug.Log("Jump Released");
+        _kart.StopDrift();
+        _kart.IsHoldingDrift = false;
+    }
+
+    public void OnThrottlePressed(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnThrottlePressed");
+        _kart.OnJump();
+        _kart.IsHoldingDrift = true;
+    }
+    public void OnThrottleReleased(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnThrottleReleased");
         _kart.StopDrift();
         _kart.IsHoldingDrift = false;
     }
